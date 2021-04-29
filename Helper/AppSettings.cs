3ff252e5114be73ef03c83b576b953bf348c9eba
Saleh Ebrahimian <salehebrahimian@gmail.com>
2021-04-29 14:58:40 +0430

    Appsetting tostring added

--- Helper/AppSettings.cs
+++ Helper/AppSettings.cs
@@ -41,6 +41,11 @@ namespace virgollanding.Helper
             }
         }
 
+        public override string ToString()
+        {
+            return string.Format("{0}\n" , JWTSecret);
+        }
+
 
     }
 }
